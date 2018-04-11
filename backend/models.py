09ddd5cf75148025575384e1e26d60b1b4612bae
ladit <ladit@qq.com>
2018-04-12 00:42:07 +0800

    finish part of admin setting schedule logic

--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+
+class Schedule(models.Model):
+    create_time = models.DateTimeField(auto_now_add=True)
+    finish_time = models.DateTimeField(null=True, blank=True)
+    finish_count = models.IntegerField(default=0)
+    websites = models.CharField(max_length=200)
+    every_x_days = models.IntegerField(default=-1)
+    every_x_clock = models.IntegerField(default=-1)
+
+
+class Job(models.Model):
+    job_name = models.TextField()  # 工作名称
+    job_pay = models.TextField()  # 工资
+    job_workplace = models.TextField()  # 工作地点
+    job_dec = models.TextField()  # 工作描述（技能提取）
+    job_min_edu = models.TextField()  # 最低学历
+    job_exp = models.TextField()  # 工作经验
+    company_welfare = models.TextField()  # 公司福利
+    company_name = models.TextField()  # 公司名称
+    company_ind = models.TextField()  # 公司行业
+    company_size = models.TextField()  # 公司规模
+    job_url = models.TextField()  # 工作详情页链接
+    job_issue = models.TextField()  # 工作来源 zlzp、51job等
+
+
+class HandledJob(models.Model):
+    job_name = models.TextField()
+    job_pay = models.IntegerField(default=-1)
+    job_workplace = models.IntegerField(default=-1)
+    job_dec = models.TextField()
+    job_min_edu = models.IntegerField(default=-9)
+    job_exp = models.IntegerField(default=-9)
+    company_welfare = models.TextField()
+    company_name = models.TextField()
+    company_ind = models.TextField()
+    company_size = models.TextField()
+    job_url = models.TextField()
+    job_issue = models.TextField()
--- backend/views.py
+++ backend/views.py
@@ -1,6 +1,87 @@
+import json
+from builtins import list
+
 from django.http import JsonResponse
+from django.conf import settings
+from django.forms.models import model_to_dict
+
+from .models import Schedule, Job, HandledJob
 
 
-def test(request):
-    test_json = {'hello': 'world', 'this': 'is test'}
-    return JsonResponse(test_json)
+def login(request):
+    if request.method == 'POST':
+        request_json = json.loads(request.body.decode('utf-8'))
+        if 'username' in request_json \
+                and 'password' in request_json \
+                and request_json['username'] == settings.ADMIN_USERNAME \
+                and request_json['password'] == settings.ADMIN_PASSWORD:
+            response = {
+                'error': 0,
+                'message': 'auth success'
+            }
+            return JsonResponse(response)
+        response = {
+            'error': -1,
+            'message': 'auth failed'
+        }
+        return JsonResponse(response)
+
+
+def schedule(request):
+    # get schedule status
+    if request.method == 'GET':
+        try:
+            first_schedule = Schedule.objects.get(pk=1)
+        except Schedule.DoesNotExist:
+            response = {
+                'error': 1,
+                'message': 'need init'
+            }
+            return JsonResponse(response)
+        else:
+            latest_schedule = Schedule.objects.latest('id')
+            if latest_schedule.finish_time is None:
+                response = {
+                    'error': 2,
+                    'message': 'latest task not finished'
+                }
+                return JsonResponse(response)
+            else:
+                all_schedules = list(Schedule.objects.all().values())
+                response = {
+                    'error': 0,
+                    'message': 'allow operation',
+                    'schedule': all_schedules
+                }
+                return JsonResponse(response)
+
+    # modify schedule
+    if request.method == 'PUT':
+        request_json = json.loads(request.body.decode('utf-8'))
+        if 'websites' in request_json \
+                and 'every_x_days' in request_json \
+                and 'every_x_clock' in request_json:
+            websites_string = ','.join(request_json['websites'])
+            new_schedule = Schedule(websites=websites_string, every_x_days=request_json['every_x_days'],
+                                    every_x_clock=request_json['every_x_clock'])
+            new_schedule.save()
+            response = {
+                'error': 0,
+                'message': 'done',
+                'schedule': model_to_dict(new_schedule)
+            }
+            return JsonResponse(response)
+        response = {
+            'error': 400,
+            'message': 'bad input'
+        }
+        return JsonResponse(response)
+
+
+def init(request):
+    # do something
+    response = {
+        'error': 0,
+        'message': 'initializing'
+    }
+    return JsonResponse(response)
