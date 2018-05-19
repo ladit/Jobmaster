36250f22cce10ac1f9fa30c3cf16520d93333381
ladit <ladit@qq.com>
2018-05-19 11:01:40 +0800

    allow CORS_ORIGIN

--- jobmaster/settings.py
+++ jobmaster/settings.py
@@ -37,11 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'backend',
     'django_rq',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -158,6 +160,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'frontend/dist/static'),
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ADMIN_USERNAME = 'admin'
 
